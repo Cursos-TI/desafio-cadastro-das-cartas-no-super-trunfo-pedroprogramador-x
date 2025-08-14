38cb6254a609f38d7e158f69c38155cf0462898a
pedroprogramador-x <pedrophbezerra@gmail.com>
2025-08-14 13:27:01 +0000

    teste i

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+int main() {
+// Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    
+    // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
---
+++ super-trunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int carta;
+    printf("Digite sua carta: \n");
+    scanf("%d", &carta);
+    printf("Sua idade é: %d\n", carta);
+
+
+
+
+
+
+
+
+
+}
